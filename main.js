467dff83d47800bbb7971c713879f01dfef176ad
yasin.gunes <yasin01ysn@gmail.com>
2024-06-08 13:04:09 +0300

    Fixed CPS middleware error

--- main.js
+++ main.js
@@ -10,39 +10,50 @@ const db = require("./services/connect");
 const sellCaffeeController = require("./controllers/sellCaffeeController");
 const userController = require("./controllers/userController");
 
+// Load environment variables from .env file
+dotenv.config();
 
-// middlewares
+// Middlewares
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 });
 
+// Use Helmet for security headers
 app.use(helmet());
-app.use(helmet.contentSecurityPolicy());        // İçerik Güvenlik Politikası (CSP)
-app.use(helmet.dnsPrefetchControl());           // DNS Ön Bulma Kontrolü
-app.use(helmet.expectCt());                     // Expect-CT
-app.use(helmet.frameguard());                   // Frameguard
-app.use(helmet.hidePoweredBy());                // Powered-By başlığını gizle
-app.use(helmet.hsts());                         // Strict-Transport-Security (HSTS)
-app.use(helmet.ieNoOpen());                     // IE Açmama
-app.use(helmet.noSniff());                      // X-Content-Type-Options: nosniff
-app.use(helmet.permittedCrossDomainPolicies()); // İzin Verilen Çapraz Domain Politikaları
-app.use(helmet.referrerPolicy());               // Referrer-Policy
-app.use(helmet.xssFilter());                    // XSS filtresi
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "trusted-cdn.com"],
+            objectSrc: ["'none'"],
+            upgradeInsecureRequests: true
+        }
+    })
+); // İçerik Güvenlik Politikası (CSP)
 
+app.use(helmet.dnsPrefetchControl()); // DNS Ön Bulma Kontrolü
+app.use(helmet.expectCt()); // Expect-CT
+app.use(helmet.frameguard()); // Frameguard
+app.use(helmet.hidePoweredBy()); // Powered-By başlığını gizle
+app.use(helmet.hsts()); // Strict-Transport-Security (HSTS)
+app.use(helmet.ieNoOpen()); // IE Açmama
+app.use(helmet.noSniff()); // X-Content-Type-Options: nosniff
+app.use(helmet.permittedCrossDomainPolicies()); // İzin Verilen Çapraz Domain Politikaları
+app.use(helmet.referrerPolicy()); // Referrer-Policy
+app.use(helmet.xssFilter()); // XSS filtresi
 
 app.use(morgan("common"));
 app.use(express.json());
 app.use(
-    cors(
-        {origin: "http://localhost:8800,"}
-        )
-    );
+    cors({
+        origin: "http://localhost:8800",
+        credentials: true
+    })
+);
 
 app.use("/api/auth", userController);
 app.use("/api/buy", sellCaffeeController);
-
-
 
 app.listen(8800, () => {
     console.log("API working!");
